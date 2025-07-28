1ad5eb78a11582a5e17b18a5ede1bf3a0c7ab29e
Blastonn <felipe.paulo07@hotmail.com>
2025-07-28 12:51:29 -0300

    Feat - Adicionar funcao de criar jogadores
    Adicionada a funcao de frabrica que cria jogadores

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 function gameboard(){
-   const posicao = [1,2,3,4,5,6,7,8,9];
-
-   return () =>{
+   const posicao = [0,1,2,3,4,5,6,7,8,9];
         return{
             tabuleiro: posicao,
             getIndice: (indice) => posicao[indice]
-
         };
-   }
 }
 
 const escolherPos = gameboard();
 
-console.log(escolherPos().getIndice(3));
+console.log(escolherPos.getIndice());
 
 function createPlayer(nome, time){
-
+    const player = "Jogador:" + nome;
+    return {player, time};
 }
+
+const newPlayer = createPlayer("Felipe", "X");
+const newPlayer2 = createPlayer("Joao", "O");
+
+
+console.log(newPlayer);
+console.log(newPlayer2);
