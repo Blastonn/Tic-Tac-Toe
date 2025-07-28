481492c002c50221239efe6d8b2057989157ef9d
Blastonn <felipe.paulo07@hotmail.com>
2025-07-28 14:02:47 -0300

    Feat - Adicionar funcao de controle de display
    Adicionada a funcao do controle de display que
    exibe as determinadas mensagens para controle do
    usuario.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gameboard = (function (){
     const posicao = [0,1,2,3,4,5,6,7,8,9];
     const getIndice = (indice) => posicao[indice];
+    const setIndice = (indice, time) => posicao[indice] = time;
     return {getIndice};
 })();
 
@@ -11,9 +12,29 @@ function createPlayer(nome, time){
     return {player, time};
 }
 
-const newPlayer = createPlayer("Felipe", "X");
-const newPlayer2 = createPlayer("Joao", "O");
+
+const controleDisplay = (function (){
+    let player1,player2;
+    const definirJogadores = () =>{
+        const jogador1 = prompt("Digite o primeiro Jogador: ");
+        player1 = createPlayer(jogador1,"X");
+        const jogador2 = prompt("Digite o segundo Jogador: ");
+        player2 = createPlayer(jogador2,"O");
+        return player1, player2;
+    }
+    const getPlayers = () =>{
+        return {player1, player2};
+    }
+    const definirPosicao = () =>{
+        const posicao = prompt("Digite a posicao: ");
+        const posicaoEsc = gameboard.getIndice(posicao);
+        console.log(posicaoEsc);
+        return posicaoEsc;
+    }
+    return {definirJogadores,definirPosicao,getPlayers};
+})();
 
 
-console.log(newPlayer);
-console.log(newPlayer2);
+controleDisplay.definirJogadores();
+controleDisplay.definirPosicao();
+controleDisplay.getPlayers();
